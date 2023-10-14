fac022dd6f61b0c347e324bfe0b2d31e108956b0
Kyrsten Júnior <kyrstenjr@gmail.com>
2023-10-13 23:48:28 -0300

    finalização do projeto

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+
+export class CardComponent {
+  @Input() gameCover: string = "";
+  @Input() gameLabel: string = "";
+  @Input() gameType: string = "";
+  @Input() gamePrice: string = "";
+}
